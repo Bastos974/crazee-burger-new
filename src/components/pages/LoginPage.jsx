e3d414c287823473cef76e1495e660e087378e1b
Bastos974 <sebastien.lemoine974@gmail.com>
2023-03-29 18:05:30 +0400

    ✨ F01: LOGIN > add form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react"
 
 
 export default function LoginPage() {
 
   //Etat Donnée
+  const [inputValue, setInput] = useState("")
 
   //comportement
+  const handleClick = (e) => {
+    console.log(e)
+    //setInput(e.target.value)
+  }
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const handlesubmit= (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`)
+    setInput("")
+  }
+
 
   //affichage
   return (
-    <div>LoginPage</div>
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handlesubmit} r>
+        <input type="text" placeholder="Entrez votre prénom" value={inputValue}
+        onChange={handleChange} required/>
+        <button onClick={handleClick}>Accédez a votre espace</button>
+      </form>
+    </div>
   )
 }
 
